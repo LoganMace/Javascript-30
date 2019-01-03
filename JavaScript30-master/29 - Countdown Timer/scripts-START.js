23fbc8c0fa5936629a6661851bf94590dfb05b7b
Logan Mace <lmmace92@hotmail.com>
2019-01-03 16:13:00 -0600

    finished day 29

--- JavaScript30-master/29 - Countdown Timer/scripts-START.js
+++ JavaScript30-master/29 - Countdown Timer/scripts-START.js
@@ -1,11 +1,16 @@
 let countdown;
 const timerDisplay = document.querySelector('.display__time-left');
+const endTime = document.querySelector('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
+  clearInterval(countdown);
+
   const now = Date.now();
   const then = now + seconds * 1000;
 
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000);
@@ -28,6 +33,28 @@ function displayTimeLeft(seconds) {
     secs = `0${secs}`;
   }
   console.log({minutes, secs});
-  const display = `${minutes}:${secs}`
+  const display = `${minutes}:${secs}`;
+  document.title = display;
   timerDisplay.textContent = display;
 };
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hours = end.getHours();
+  const mins = end.getMinutes();
+  endTime.textContent = `Be Back at ${hours > 12 ? hours - 12 : hours}:${mins < 10 ? '0' : ''}${mins}`;
+};
+
+function startTimer(e) {
+  const seconds = +this.dataset.time;
+  console.log('seconds: ', seconds);
+  timer(seconds);
+};
+
+buttons.forEach(btn => btn.addEventListener('click', startTimer));
+document.customForm.addEventListener('submit', function(e) {
+  e.preventDefault();
+  const mins = this.minutes.value;
+  this.reset();
+  timer(mins * 60);
+});
